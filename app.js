4481dd1b89096587ea2663173739e0d41d85153d
Fernando <fernando.fsb24@gmail.com>
2022-07-01 15:21:10 -0400

    registro y login funcionando
    registro de usuario y login funcionando de forma correcta

--- app.js
+++ app.js
@@ -48,13 +48,33 @@ app.get('/login',(req, res)=>{
 app.get('/register',(req, res)=>{
     res.render('register');
 })
+app.get('/index',(req, res)=>{
+    res.render('index');
+})
 
 //registros
 app.post('/register', async (req ,res)=>{
     const user = req.body.nombre;
     const email = req.body.email;
     const pass = req.body.password;
-    
+    const Repass = req.body.Repassword;
+
+    connection.query('SELECT * FROM users WHERE email = ?',[email], async(error,results)=>{
+        if(results.length !=0 ){
+            res.render('register',{
+                alert:true,
+                alertTitle:"Error",
+                alertMessage:"Email ya existente",
+                alertIcon:'error',
+                showConfirmButton:false,
+                timer:2500,
+                ruta:''
+            })
+        }
+    });
+
+    if(pass == Repass){
+        console.log('contraseñas iguales');
     let passwordHaash = await bcryptjs.hash(pass, 8);
     
     connection.query('INSERT INTO users SET ?',{name:user,email:email,password:passwordHaash},async(error, results)=>{
@@ -68,27 +88,76 @@ app.post('/register', async (req ,res)=>{
                 alertIcon:'success',
                 showConfirmButton:false,
                 timer:2500,
-                ruta:''
+                ruta:'/login'
             });
+            console.log(results);
+            console.log(error);
         }
-    })
+    })}else{
+        res.render('register',{
+            alert:true,
+            alertTitle:"Error",
+            alertMessage:"Las contraseñas deben ser iguales",
+            alertIcon:'error',
+            showConfirmButton:false,
+            timer:2500,
+            ruta:''
+        })
+    }
 
 })
 
 //login
 app.post('/auth', async(req ,res)=>{
     const email =req.body.email;
-    const pass =req.body.password;
+    const password =req.body.password;
 
-    let passwordHaash = await bcryptjs.hash(pass, 8);
-     if(email && pass){
-        connection.query('SELECT * FROM users WHERE email = ?',[email], async(error,results)=>{
-            // console.log(results);
-            if(!(passwordHaash.localeCompare(pass,results[0].pass))){
-                res.send('usuario Y/O password incorrecto');
-            }else{
-                res.send('Login correcto');
+     if(email && password){
+         connection.query(`SELECT * FROM users WHERE email = ?`,[email], async(error,results)=>{
+
+         if(results.length != 0){
+                console.log('primer if')
+                if(await bcryptjs.compare(password, results[0].password)){
+                    console.log('primer await')
+                    console.log(results);
+                    console.log(error);
+                    req.session.name = results[0].name;
+                    res.render('login',{
+                        alert:true,
+                        alertTitle:"Bienvenido",
+                        alertMessage:"Logeado con exito",
+                        alertIcon:"success",
+                        showConfirmButton:true,
+                        timer:1500,
+                        ruta:'index'
+                    });
+
+                    
+                }else{
+                    res.render('login',{
+                        alert:true,
+                        alertTitle:"Error",
+                        alertMessage:"Contraseña incorrecta",
+                        alertIcon:"error",
+                        showConfirmButton:true,
+                        timer:false,
+                        ruta:'login'
+                    });
+                console.log('contraseña incorrecta')
             }
+         }else{
+            res.render('login',{
+                alert:true,
+                alertTitle:"Error",
+                alertMessage:"Usuario incorrecto",
+                alertIcon:"error",
+                showConfirmButton:true,
+                timer:false,
+                ruta:'login'
+            });
+                console.log('error usurio incorrecto');
+                console.log(results);
+        }
         })
      }
 
@@ -98,5 +167,7 @@ app.post('/auth', async(req ,res)=>{
 app.listen(3000,(req, res)=>{
     console.log('SERVER RUNNING IN http://localhost:3000');
 })
+
+
 
 
